323d6dc485e364ee15ef3529f8977cf954a0c1d6
Brian Q Anderson <bqanderson@me.com>
2018-08-13 21:59:29 -0500

    Way to long between commits commit.

---
+++ src/app/character.ts
@@ -0,0 +1,8 @@
+export class Character {
+  id: number;
+  name: string;
+  charClass: string;
+  level: number;
+  armorClass: number;
+  hitPoints: number;
+}
--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../character';
 
 @Component({
   selector: 'app-characters',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./characters.component.scss']
 })
 export class CharactersComponent implements OnInit {
+
+  character: Character = {
+    id: 1,
+    name: 'Brother Tiberius',
+    charClass: 'Cleric',
+    level: 8,
+    armorClass: 17,
+    hitPoints: 68
+  };
 
   constructor() { }
 
